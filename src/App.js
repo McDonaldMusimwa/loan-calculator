28dff07961d0d94e04a9bcfb86772f87b6608cb3
McDonald <mus22003@byui.edu>
2023-11-04 02:33:42 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React ,{useState} from 'react';
+
+import React, { useState } from "react";
 import Form from "./components/Form";
 import Table from "./components/table";
 import Header from "./components/header";
 
-
 function App() {
-  const [calculatedData,setCalculatedData]= useState([])
+  const [calculatedData, setCalculatedData] = useState([]);
   const calculateHandler = (userInput) => {
-  
     // Should be triggered when form is submitted
     // You might not directly want to bind it to the submit event on the form though...
 
@@ -17,9 +16,7 @@ function App() {
     const yearlyContribution = +userInput.contribution; // as mentioned: feel free to change the shape...
     const expectedReturn = +userInput.expectedreturn / 100;
     const duration = +userInput.duration;
-   
 
-    
     // The below code calculates yearly results (total savings, interest etc)
     for (let i = 0; i < duration; i++) {
       const yearlyInterest = currentSavings * expectedReturn;
@@ -32,11 +29,10 @@ function App() {
         savingsEndOfYear: currentSavings,
         yearlyContribution: yearlyContribution,
       });
-
-      
     }
-    
-   setCalculatedData(yearlyData)
+    console.log(yearlyData);
+
+    setCalculatedData(yearlyData);
     // do something with yearlyData ...
   };
 
@@ -47,7 +43,7 @@ function App() {
       <Form calculate={calculateHandler} />
       {/* Todo: Show below table conditionally (only once result data is available) */}
       {/* Show fallback text if no data is available */}
-      <Table yearlyData={calculatedData}/>
+      <Table yearlyData={calculatedData} />
     </div>
   );
 }
